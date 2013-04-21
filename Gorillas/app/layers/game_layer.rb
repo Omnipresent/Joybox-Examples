a2b51f8d17e1bd2c46b20f2054b1c559b764dc84
bhaarat <bhaarat@omnipresent.local>
2013-04-21 01:28:21 -0400

    move player gorilla

--- Gorillas/app/layers/game_layer.rb
+++ Gorillas/app/layers/game_layer.rb
@@ -9,28 +9,30 @@ class GameLayer < Joybox::Core::Layer
       @world.step delta: dt
 
     end
+    # this should ideally be a PhysicsSprite so player can catch bananas. 
+    # Moving PhysicsSprite will soon be added to JoyBox!
+    @player_gorilla = Sprite.new file_name: 'gorilla.png', 
+                                  position: [248, 224]
 
-    @player_gorilla = Sprite.new file_name: 'gorilla.png',
-                                  position: [108, 147]
+    bottom_layer = @world.new_body position: [0, 0], 
+                            type: KStaticBodyType do
+
+      edge_fixure start_point: CGPointMake(0,0), end_point: CGPointMake(590, 0),
+                     friction: 0.3,
+                     density: 0.5
+    end
+    @hero_gorilla = Sprite.new file_name: 'gorilla.png',position: [125, 35]
+    @layer = PhysicsSprite.new file_name: 'greenstrip.png', body: bottom_layer
 
     self << @player_gorilla
+    self << @layer
 
-    body = @world.new_body position: [389, 154] do
-
-      polygon_fixure box: [16, 16],
-                     friction: 0.3,
-                     density: 1.0
-    end
-
-    @enemy_gorilla = PhysicsSprite.new file_name: 'gorilla.png',
-                                            body: body
-
-    self << @enemy_gorilla
+    self << @hero_gorilla
 
     init_controls
   end
 
-    def init_controls
+  def init_controls
 
     on_touches_began do |touches, event|
 
@@ -40,18 +42,12 @@ class GameLayer < Joybox::Core::Layer
     end
 
 
-#    on_touches_ended do |touches, event|
-#
-#      end_touch = touches.any_object
-#
-#      end_touch_location = end_touch.location
-#
-#      banana = new_banana_sprite
-#
-#      self << banana
-#
-#      banana.body.apply_force force: (end_touch_location - @starting_touch_location)
-#    end
+
+    on_touches_ended do |touches, event|
+      end_touch = touches.any_object
+      end_touch_location = end_touch.location
+      @hero_gorilla.run_action Move.to duration: 1, position: [end_touch.location.x, 35]
+    end
 
     @duration = 0
     @timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:'throwBanana', userInfo:nil, repeats:true)
@@ -59,30 +55,32 @@ class GameLayer < Joybox::Core::Layer
 
   def throwBanana
     @duration = @duration + 1
-    puts "entered here : " + @duration.to_s
     banana = new_banana_sprite
     self << banana
-    banana.body.apply_force force: (CGPointMake(rand(320),120))
+    y = rand(80)
+    x = rand(100)-50
+    banana.body.apply_force force: (CGPointMake(x,y))
   end
 
   def new_banana_sprite
-
     banana_body = @world.new_body position: @player_gorilla.position,
                                       type: KDynamicBodyType do
-
-        polygon_fixure box: [16, 16],
-                       friction: 0.3,
-                       density: 1.0
+        polygon_fixure box: [6, 6],
+                       friction: 10.8,
+                       density: 2.5
+                       #,restitution: 0.5
     end 
 
     @banana_sprite = PhysicsSprite.new file_name: 'banana.png',
                                             body: banana_body
 
-    @world.when_collide banana_body do |collision_body, is_touching|
+    #Collision is removed for now but will be added when player_gorilla can catch bananas
+   # @world.when_collide banana_body do |collision_body, is_touching|
 
-      @banana_sprite.file_name = 'banana_hit.png'
-      @enemy_gorilla.file_name = 'gorilla_hit.png'
-    end
+   #   @banana_sprite.file_name = 'banana_hit.png'
+      #@enemy_gorilla.file_name = 'gorilla_hit.png'
+      #@hero_gorilla.file_name = 'gorilla_hit.png'
+   # end
 
     @banana_sprite
   end
